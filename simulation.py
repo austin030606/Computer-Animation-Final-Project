c332f6892176e5c15f5367d4401e9e63704c6f73
Emeraldb01 <emeraldb01.cs09@nycu.edu.tw>
2023-05-27 14:22:51 +0800

    Finish sim.py "Handle", graph.py "update_frame"

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 
 class Simulation:
     def __init__(self, row=100, col=100):
@@ -20,6 +21,7 @@ class Fluid(Simulation):
         self.density = np.zeros(shape)
         self.velocity = np.zeros(shape)
         self.data = self.density # set the data for displaying purposes
+        self.forceStrength = 10 # Amount of force add per click
         pass
 
     def densityUpdate(self):
@@ -34,6 +36,12 @@ class Fluid(Simulation):
         self.densityUpdate()
         self.velocityUpdate()
 
-    def Handle(event, row=0, col=0):
+    def Handle(self, event, mousePos, cellSize):
         # handle fluid states based on event type and mouse position
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Convert the mouse position to array indices
+            col = mousePos[0] // cellSize
+            row = mousePos[1] // cellSize
+            
+            # Add force at mouse position
+            self.data[row][col] += self.forceStrength
